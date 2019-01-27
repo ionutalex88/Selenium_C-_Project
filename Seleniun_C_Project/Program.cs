89b2f7a9df04c5400a1c9e461d1ec3ca39400936
ionutalex88 <truscaionutalex@gmail.com>
2019-01-28 00:38:07 +0200

    Added first changes Test

--- Seleniun_C_Project/Program.cs
+++ Seleniun_C_Project/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,45 @@ namespace Seleniun_C_Project
 {
     class Program
     {
+
+        //Create the reference for browser
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
+            
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("http://www.google.ro");
+            Console.WriteLine("Open URL");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //EnterText(element, value, type)
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("execute C# selenium test");
+
+            Console.WriteLine("Executed test");
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            Console.WriteLine("Second Test");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            driver.Quit();
+            Console.WriteLine("Closed Browser");
         }
     }
 }
---
+++ Seleniun_C_Project/SeleniumSetMethods.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seleniun_C_Project
+{
+    class SeleniumSetMethods
+    {
+        //Enter Text
+        public static void EnterText(IWebElement element, string value, string elementType)
+        {
+
+            //Test
+        }
+
+    }
+}
